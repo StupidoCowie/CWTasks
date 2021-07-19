3bd95ae0885e6e3220b152fbb0052f0870b88c75
StupidoCowie <nikton455@gmail.com>
2021-07-19 12:29:47 +0500

    Added Cancellation

--- CWTasks/Windows/TaskFourWindow.xaml.cs
+++ CWTasks/Windows/TaskFourWindow.xaml.cs
@@ -18,13 +18,14 @@ namespace CWTasks.Windows
         private int _count, _test;
         private Random _random;
         private CancellationTokenSource _cancel;
+        private TaskStatus _currentTaskStatus;
         public TaskFourWindow()
         {
             InitializeComponent();
             _random = new Random();
             _task = new TaskFour();
             _test = 0;
-            _cancel = new CancellationTokenSource();
+            _currentTaskStatus = TaskStatus.WaitingForActivation;
         }
 
         private async void GenerateAndSort_Click(object sender, RoutedEventArgs e)
@@ -33,49 +34,51 @@ namespace CWTasks.Windows
             Output.Text = "Loading...";
             if (int.TryParse(Count.Text, out _count) && _count > 0)
             {
+                _cancel = new CancellationTokenSource();
                 GenerateAndSort.IsEnabled = false;
-                await Task.Run(() =>
+                Task currentTask = Task.Run(() =>
                 {
+                    _currentTaskStatus = TaskStatus.Running;
                     _input = new int[_count];
                     _output = new int[_count];
                     string TempString = "";
                     for (int i = 0; i < _count; i++)
                     {
-                        //try
-                        //{
-                        //    _cancel.Token.ThrowIfCancellationRequested();
-                        //}
-                        //catch (Exception exp)
-                        //{
-                        //    MessageBox.Show(exp.Message, "Warning");
-                        //}
+                        if (_cancel.IsCancellationRequested)
+                        {
+                            break;
+                        }
                         _input[i] = _random.Next(0, 9);
                         TempString += $"{_input[i]} ";
                     }
-                    Input.Dispatcher.Invoke(() =>
+                    if (!_cancel.IsCancellationRequested)
                     {
-                        Input.Text = TempString;
-                        Output.Text = "Calculating...";
-                    });
+                        Input.Dispatcher.Invoke(() =>
+                        {
+                            Input.Text = TempString;
+                            Output.Text = "Calculating...";
+                        });
+                    }
                     TempString = "";
                     _output = _task.SortArray(_input);
                     foreach (int i in _output)
                     {
-                        //try
-                        //{
-                        //    _cancel.Token.ThrowIfCancellationRequested();
-                        //}
-                        //catch (Exception exp)
-                        //{
-                        //    MessageBox.Show(exp.Message, "Warning");
-                        //}
+                        if (_cancel.IsCancellationRequested)
+                        {
+                            break;
+                        }
                         TempString += $"{i} ";
                     }
-                    Output.Dispatcher.Invoke(() =>
+                    if (!_cancel.IsCancellationRequested)
                     {
-                        Output.Text = TempString;
-                    });
+                        Output.Dispatcher.Invoke(() =>
+                        {
+                            Output.Text = TempString;
+                        });
+                    }
                 }, _cancel.Token);
+                await currentTask;
+                _currentTaskStatus = currentTask.Status;
                 GenerateAndSort.IsEnabled = true;
             }
             else
@@ -89,12 +92,30 @@ namespace CWTasks.Windows
             _test += 1;
             TestCount.Text = _test.ToString();
         }
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentTaskStatus.Equals(TaskStatus.Running))
+            {
+                _cancel.Cancel();
+                Input.Text = "Cancelled";
+                Output.Text = "Cancelled";
+                MessageBox.Show("Current task has been cancelled", "Info");
+                GenerateAndSort.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("There is nothing to cancel", "Info");
+            }
+        }
 
         private void ReturnToTaskWindow_Click(object sender, RoutedEventArgs e)
         {
+            if (_currentTaskStatus.Equals(TaskStatus.Running))
+            {
+                Cancel_Click(sender, e);
+            }
             _taskWindow = new TasksWindow();
             _taskWindow.Show();
-            _cancel.Cancel();
             Close();
         }
     }
